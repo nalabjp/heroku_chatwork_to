4d8a3946f7da2fbaabeaec1b7726cbe9e8b62368
nalabjp <nalabjp@gmail.com>
2014-08-20 13:44:56 +0900

    Added ROOMS_5

--- heroku_config.sh
+++ heroku_config.sh
@@ -5,4 +5,5 @@ heroku config:add CW2_ROOMS_1=$CW2_ROOMS_1
 heroku config:add CW2_ROOMS_2=$CW2_ROOMS_2
 heroku config:add CW2_ROOMS_3=$CW2_ROOMS_3
 heroku config:add CW2_ROOMS_4=$CW2_ROOMS_4
+heroku config:add CW2_ROOMS_5=$CW2_ROOMS_5
 heroku config:add CW2_SLACK_TOKEN=$CW2_SLACK_TOKEN
