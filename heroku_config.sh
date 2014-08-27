100b157403b46e3fa11502164bff2aadb57026f6
nalabjp <nalabjp@gmail.com>
2014-08-27 14:23:27 +0900

    Replace array via erb in yaml

--- heroku_config.sh
+++ heroku_config.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 heroku config:add CW2_EMAIL=$CW2_EMAIL
 heroku config:add CW2_PASS=$CW2_PASS
-heroku config:add CW2_ROOMS_1=$CW2_ROOMS_1
-heroku config:add CW2_ROOMS_2=$CW2_ROOMS_2
-heroku config:add CW2_ROOMS_3=$CW2_ROOMS_3
-heroku config:add CW2_ROOMS_4=$CW2_ROOMS_4
-heroku config:add CW2_ROOMS_5=$CW2_ROOMS_5
+heroku config:add CW2_ROOMS=$CW2_ROOMS
 heroku config:add CW2_SLACK_TOKEN=$CW2_SLACK_TOKEN
 
 heroku config:add CW_CHATWORK_TOKEN=$CW_CHATWORK_TOKEN
